fc1961990c6a83d5a365fc931805837a5be6cdb6
Dejan Vasic <dejanvasic24@gmail.com>
2019-01-30 15:39:07 +1100

    Implementing the auth service based on auth0 documentation

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,7 +1,12 @@
 import auth0 from 'auth0-js';
 import {appConfig} from './config';
+import history from './history';
 
 export default class Auth {
+  accessToken;
+  idToken;
+  expiresAt;
+
   auth0 = new auth0.WebAuth({
     domain: appConfig.domain,
     clientID: appConfig.clientId,
@@ -10,7 +15,64 @@ export default class Auth {
     scope: 'openid'
   });
 
-  login() {
+  login = () => {
     this.auth0.authorize();
+  }
+
+  setSession = (authResult) => {
+    localStorage.setItem('isLoggedIn', true);
+
+    // Set the time that the access token will expire at
+    let expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
+    this.accessToken = authResult.accessToken;
+    this.idToken = authResult.idToken;
+    this.expiresAt = expiresAt;
+
+    // navigate to the home route
+    history.replace('/');
+  }
+
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+      } else if(err) {
+        history.replace('/');
+        console.log('err', err);
+        alert('boom, something went wrong');
+      }
+    });
+  }
+
+  renewSession = () => {
+    this.auth0.checkSession({}, (err, authResult) => {
+       if (authResult && authResult.accessToken && authResult.idToken) {
+         this.setSession(authResult);
+       } else if (err) {
+         this.logout();
+         console.log(err);
+         alert(`Could not get a new token (${err.error}: ${err.error_description}).`);
+       }
+    });
+  }
+
+  logout = () => {
+    // Remove tokens and expiry time
+    this.accessToken = null;
+    this.idToken = null;
+    this.expiresAt = 0;
+
+    // Remove isLoggedIn flag from localStorage
+    localStorage.removeItem('isLoggedIn');
+
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  isAuthenticated = () => {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = this.expiresAt;
+    return new Date().getTime() < expiresAt;
   }
 }
