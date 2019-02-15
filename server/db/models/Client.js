3d9f50e8d3700739df829eff00112d37a72d4048
Dejan Vasic <dvasic@seek.com.au>
2019-02-15 19:56:08 +1100

    Creating first migration and reconfiguring sequelize cli

--- server/db/models/Client.js
+++ server/db/models/Client.js
@@ -1,18 +1,15 @@
-const Sequelize = require('sequelize');
-
-const User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  },
-  email : {
-    type: Sequelize.STRING
-  },
-  phone: {
-    type: Sequelize.STRING
-  }
-});
-
-module.exports = User;
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Client = sequelize.define('Client', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {});
+  Client.associate = function(models) {
+    // associations can be defined here
+  };
+  return Client;
+};
