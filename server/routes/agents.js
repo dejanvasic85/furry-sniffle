41afd1a37293203068e12242b6986ac685bb2c6c
Dejan Vasic <dvasic@seek.com.au>
2019-02-25 20:17:20 +1100

    Adding endpoint for updating agents

--- server/routes/agents.js
+++ server/routes/agents.js
@@ -4,11 +4,6 @@ const { db, Agent } = require('../db');
 const Sequelize = require('sequelize');
 
 router.post('/', (req, res) => {
-  if (!req.body) {
-    res.status(401).json({ message: 'missing client details in request bodyu' });
-    return;
-  }
-
   const newAgent = Object.assign({}, req.body, { agentId: req.agentId });
   Agent.findOrCreate({
     where: {
@@ -20,6 +15,25 @@ router.post('/', (req, res) => {
       res.status(400).json({ error: 'Agent with email already registered' });
     }
     res.status(201).json(agent);
+  }).catch(err => {
+    res.status(500).json(err);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  console.log(`agents/update/${ req.params.id }`);
+  const { firstName } = req.body;
+
+  Agent.update({ firstName: firstName }, {
+    where: {
+      id: req.params.id
+    },
+    returning: true
+  }).spread((recordsAffected, result) => {
+    if (recordsAffected === 0) {
+      res.status(400).json({ error: 'Update failed' });
+    }
+    res.status(200).json(result);
   }).catch(err => {
     res.status(500).json(err);
   });
