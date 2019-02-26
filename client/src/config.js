b2d04464f9c622f5767b029f2568619fbd3515a3
Dejan Vasic <dvasic@seek.com.au>
2019-02-26 23:43:40 +1100

    Got the proxy to work

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const appConfig = {
-  apiBaseUrl: 'http://localhost:5000',
+  apiBaseUrl: '/api',
   domain: 'paramount.au.auth0.com',
   clientId: 'vBP5pNdN3pg0AF7cPTd6UlqolKxbD335',
   callbackUrl: 'http://localhost:3000/callback'
