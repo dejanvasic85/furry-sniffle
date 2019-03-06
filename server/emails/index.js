24b9aa4253f8666408f0eb20c3888c2d3ff88273
Dejan Vasic <dvasic@seek.com.au>
2019-03-06 22:01:48 +1100

    Got the sendgrid working with the node library. Easier

--- server/emails/index.js
+++ server/emails/index.js
@@ -1,44 +1,24 @@
-const axios = require('axios');
-
 const { sendGrid } = require('../config');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(sendGrid.apiKey);
 
-const body = JSON.stringify({
-  "from": {
-    "email": "support@agento.com"
+const msg = {
+  to: 'dejanvasic24@gmail.com',
+  from: 'support@agento.com',
+  subject: 'You have an invitation',
+  text: 'Hello plain world!',
+  html: '<p>Hello HTML world!</p>',
+  templateId: 'd-3cd3de0b1a7345d384e9662fbd7ebbe1',
+  dynamic_template_data: {
+    agentName: 'Alex',
+    agentRewardsUrl: 'http://google.com.au',
+    clientName: 'FooBar',
+    clientReferralUrl: 'http://google.com.au'
   },
-  "personalizations": [{
-    "to": [{
-      "email": "dejanvasic24@gmail.com"
-    }],
-    "dynamic_template_data": {
-      "agentName": "Alex",
-      "clientName": "Foo",
-      "agentRewardsUrl": "http://google.com.au",
-      "clientReferralUrl": "http://google.com.au"
-    }
-  }],
-  "template_id": "d-3cd3de0b1a7345d384e9662fbd7ebbe1"
-});
-
-const sendMail = (templateId) => {
-  const axiosConfig = {
-    method: 'POST',
-    url: `${sendGrid.baseUrl}/mail/send`,
-    body: body,
-    headers: {
-      'Authorization': `Bearer ${sendGrid.apiKey}`,
-      'Content-Type': 'application/json'
-    }
-  };
-
-  console.log('sending to axios', axiosConfig);
-  return axiosConfig;
 };
-
 
 module.exports = {
   newClient: () => {
-    const newClientTemplateId = "d-3cd3de0b1a7345d384e9662fbd7ebbe1";
-    return axios(sendMail(newClientTemplateId));
+    return sgMail.send(msg);
   }
 };
