83a5d2e6f13c00a896c0855c01b67f598387794f
Alex Maslyukov <amaslyukov@seeklearning.com.au>
2019-03-13 23:38:37 +1100

    added callback URL to point to Heroku deployed app

--- client/src/auth/authConfig.js
+++ client/src/auth/authConfig.js
@@ -2,5 +2,6 @@ export const authConfig = {
   audience: 'http://localhost:5000',
   domain: 'paramount.au.auth0.com',
   clientId: 'vBP5pNdN3pg0AF7cPTd6UlqolKxbD335',
-  callbackUrl: 'http://localhost:3000/callback'
-}
+  callbackUrl:
+    process.env.REACT_APP_AUTH_CALLBACK_URL || 'http://localhost:3000/callback'
+};
