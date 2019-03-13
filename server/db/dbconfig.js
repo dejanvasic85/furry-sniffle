f405724df799f3e2833b576adbc641f8eacf3fed
Alex Maslyukov <amaslyukov@seeklearning.com.au>
2019-03-13 22:51:31 +1100

    set up dialect for Postgress

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -3,12 +3,15 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.PGUSER,
-    "password": process.env.PGPASSWORD,
-    "database": process.env.PGDATABASE,
-    "host": process.env.PGHOST,
-    "port": process.env.PGPORT,
-    "dialect": "postgres"
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  },
+  production: {
+    dialect: 'postgres'
   }
-}
+};
