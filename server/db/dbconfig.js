71003d9403b8c0a5b5b8748585fadec66dcadc98
Alex Maslyukov <maslukov@gmail.com>
2019-03-14 10:59:12 +1100

    Deployment to Heroku - API and UI
    * resolved conflicts
    
    * feat(Deploy) added Heroku DB connection URL
    
    * db
    
    * another
    
    * added DB migration and server HTML
    
    * wrong command
    
    * set up dialect for Postgress
    
    * use special flag to tell Sequelizer to use DATABASE_URL
    
    * added path
    
    * server JS files
    
    * added callback URL to point to Heroku deployed app

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -3,12 +3,16 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.PGUSER,
-    "password": process.env.PGPASSWORD,
-    "database": process.env.PGDATABASE,
-    "host": process.env.PGHOST,
-    "port": process.env.PGPORT,
-    "dialect": "postgres"
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
   }
-}
+};
