606504e7ec7ab61fa71f035096bba8afd54b246d
Alex Maslyukov <maslukov@gmail.com>
2019-03-15 16:17:43 +1100

    Fixed the auth0 configuration being loaded from .env

--- client/src/auth/AuthCallback.jsx
+++ client/src/auth/AuthCallback.jsx
@@ -8,34 +8,44 @@ class AuthCallback extends React.Component {
   state = {
     goHome: false,
     goCompleteRegistration: false
-  }
+  };
 
   componentDidMount() {
     const { auth } = this.props;
-    auth.handleAuthentication().then(authResult => {
-      apiClient.getAgent(authResult.accessToken).then(profile => {
-        if (!profile) {
-          apiClient.createAgent().then(() => {
-            this.setState({ goCompleteRegistration: true });
+    auth
+      .handleAuthentication()
+      .then(authResult => {
+        apiClient
+          .getAgent(authResult.accessToken)
+          .then(profile => {
+            if (!profile) {
+              apiClient.createAgent().then(() => {
+                this.setState({ goCompleteRegistration: true });
+              });
+            } else {
+              if (agentRequiresSetup(profile) === true) {
+                this.setState({ goCompleteRegistration: true });
+              } else {
+                this.setState({ goHome: true });
+              }
+            }
+          })
+          .catch(e => {
+            console.error(`getAgent failed`, e, authResult);
           });
-        } else {
-          if (agentRequiresSetup(profile) === true) {
-            this.setState({ goCompleteRegistration: true });
-          } else {
-            this.setState({ goHome: true });
-          }
-        }
+      })
+      .catch(e => {
+        console.error(`handleAuthentication failed`, e);
       });
-    });
   }
 
   render() {
     if (this.state.goCompleteRegistration) {
-      return <Redirect to="/app/agent/details" />
+      return <Redirect to="/app/agent/details" />;
     }
 
     if (this.state.goHome) {
-      return <Redirect to="/app" />
+      return <Redirect to="/app" />;
     }
 
     return <div>Logging in... please wait</div>;
