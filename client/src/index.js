4d7836189ec3a1a337d64b9d5f42fcaf71fdbf92
Dejan Vasic <dvasic@seek.com.au>
2019-03-15 16:51:57 +1100

    Removing the redundant login page

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import App from './App';
 import AuthCallback from './auth/AuthCallback';
-import Login from './auth/Login';
 import InvitationPage from './pages/InvitationPage';
 import AuthService from './auth/AuthService';
 import LandingPage from './pages/LandingPage';
@@ -17,7 +16,6 @@ ReactDOM.render(
   <Router>
     <div>
       <Switch>
-        <Route path="/login" render={(props) => <Login {...props} auth={authService} />} />
         <Route path="/callback" render={(props) => <AuthCallback {...props} auth={authService} />} />
         <Route path="/app" render={(props) => <App {...props} auth={authService} />} />
         <Route path="/invite/:agentId/code/:referralCode" exact render={(props) => <InvitationPage {...props} />} />
