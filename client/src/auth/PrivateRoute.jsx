9ca5db8965fe02c79aa01f76a380ef86d3573f39
Dejan Vasic <dvasic@seek.com.au>
2019-03-15 16:50:37 +1100

    Fixing the private route to auth.login() automatically

--- client/src/auth/PrivateRoute.jsx
+++ client/src/auth/PrivateRoute.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => (
-  <Route {...rest} render={(props) => (
-    auth.isAuthenticated() === true
-      ? <Component {...props} />
-      : <Redirect to="/login" />
-  )} />
-)
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+  if (auth.isAuthenticated()) {
+    return <Route {...rest} render={(props) => <Component {...props} />} />
+  }
+
+  auth.login();
+  return null;
+}
 
 export default PrivateRoute;
