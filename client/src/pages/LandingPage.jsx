9c627a48c5a8b57c4a46775432b18572f5b1ca5f
Dejan Vasic <dejanvasic24@gmail.com>
2019-03-15 16:53:45 +1100

    Fixing the PrivateRoute and removing redundant login page (#15)
    * Adjusting the order of package.json commands
    * Fixing the private route to auth.login() automatically
    * Removing the redundant login page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class LandingPage extends React.Component {
   render() {
     return <div>
-      Landing Page ... coming soon near you!
+      Landing Page ... coming soon near you! <Link to="/app">Agent Login</Link>
     </div>;
   }
 }
