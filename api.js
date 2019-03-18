daa63773c2e6d0f8f0d036d57af7f6a126df87f2
Dejan Vasic <dvasic@seek.com.au>
2019-03-19 10:14:47 +1100

    Fixing the error handling but maybe we shouldn't send the error message in the future for security purposes

--- api.js
+++ api.js
@@ -2,15 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const app = express();
-const config = require('./server/config');
+const path = require('path');
 
+const config = require('./server/config');
 const clients = require('./server/routes/clients');
 const emailWebhook = require('./server/routes/emailWebhook');
 const agents = require('./server/routes/agents');
 const prospects = require('./server/routes/prospects');
-
-const path = require('path');
-
 const { db } = require('./server/db');
 const logger = require('./server/logger');
 
@@ -18,26 +16,11 @@ const { agentAuth, jwtAuth, errorHandler } = require('./server/middleware');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  if (req.method === 'POST') {
-    logger.info(
-      `method: POST, path: ${req.path}, body: ${JSON.stringify(req.body)}`
-    );
-  }
-  next();
-});
-
-app.get('/api/health', (req, res) => {
-  res.send('ok');
-});
-
+app.get('/api/health', (req, res) => res.json({ status: 'ok' }));
 app.use('/api/prospects', prospects);
 app.use('/api/agents', agents);
 app.use('/api/email', emailWebhook);
 app.use('/api/clients', jwtAuth, agentAuth, clients);
-app.use(errorHandler);
-
 app.get('/index.html', (req, res) => {
   res.sendFile(path.join(__dirname + '/landing/index.html'));
 });
@@ -45,16 +28,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/landing/index.html'));
 });
 
-// // Serve static files from the React app
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(path.join(__dirname, 'landing')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-console.log('DB Authenticating..');
+// Error handling must come last
+app.use(errorHandler);
+
+logger.info('DB Authenticating..');
 db.authenticate().then(() => {
-  console.log('DB Authentication successful');
+  logger.info('DB Authentication successful');
   app.listen(config.portNumber, () => {
     logger.info(`Listening on port ${config.portNumber}`);
   });
