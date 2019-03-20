13222af9975c31e260fa6f546dd4034bfbd5978f
Dejan Vasic <dejanvasic24@gmail.com>
2019-03-21 10:30:31 +1100

    tech(ApiAsyncRefactor) Adding withAsync to other endpoints and cleaning up the flow of code in the API (#25)
    * Refacoring the clients route and introducing async
    * Got async to work with error handling and tested out the email templates. All working.
    * Simplifying the invite endpoint
    * Endpoint for fetching prospects for agent
    * Adding endpoint to fetch a single prospect
    * Adding update endpoint to prospects

--- server/routes/prospects.js
+++ server/routes/prospects.js
@@ -2,59 +2,111 @@ const express = require('express');
 const router = express.Router();
 
 const { Prospect, Client, Agent } = require('../db');
+const { jwtAuth, agentAuth, withAsync } = require('../middleware');
 const emailer = require('../emails');
 const logger = require('../logger');
 
-router.post('/', (req, res) => {
-  if (!req.body) {
-    res.status(400).json({ error: 'Missing body' });
-    return;
-  }
-
-  const prospect = req.body;
-  prospect.status = 'New';
-
-  Prospect.create(prospect).then(p => {
-    res.status(201).json(p);
-  }).catch(err => {
-    res.status(500).json({ err });
-  });
+const PROSPECT_STATE = Object.freeze({
+  NEW: 'New'
 });
 
-router.post('/invite', (req, res) => {
+router.get('/', jwtAuth, agentAuth, withAsync(async (req, res) => {
+  const agentId = req.agent.id;
+  logger.info(`Fetching all prospects for agentId ${agentId}`);
+  const prospects = await Prospect.findAll({
+    where: { agentId }
+  });
+
+  res.json(prospects);
+}));
+
+router.get('/:id', jwtAuth, agentAuth, withAsync(async (req, res) => {
+  const agentId = req.agent.id;
+  const id = req.params.id;
+  logger.info(`Fetching prospect id: ${id} for agentId: ${agentId}`);
+  const prospect = await Prospect.findOne({
+    where: { id, agentId }
+  });
+  if (!prospect) {
+    res.status(404).json({ error: 'Not Found' });
+    return;
+  }
+
+  res.json(prospect);
+}));
+
+router.put('/:id', jwtAuth, agentAuth, withAsync(async (req, res) => {
+  const agentId = req.agent.id;
+  const id = req.params.id;
+  const { status } = req.body;
+
+  let prospect = await Prospect.findOne({
+    where: { id, agentId }
+  })
+
+  if (!prospect) {
+    res.status(404).json({ error: 'Not Found' });
+    return;
+  }
+
+  prospect = await prospect.update({
+    status
+  });
+
+  res.status(204).json(prospect);
+}));
+
+router.post('/', withAsync(async (req, res) => {
+  if (!req.body) {
+    res.json({ error: 'Missing body' }).status(400);
+    return;
+  }
+
+  const newProspect = req.body;
+  newProspect.status = 'New'; // dv: todo - move to constants
+
+  const agent = await Agent.findOne({ where: { id: newProspect.agentId } });
+  const client = await Client.findOne({ where: { id: newProspect.clientId } });
+  const prospect = await Prospect.create(newProspect);
+  // Should we even await this?
+  await emailer.sendNewProspectEmail(newProspect, client, agent);
+  res.status(201).json({ prospect });
+}));
+
+router.post('/invite', withAsync(async (req, res) => {
   if (!req.body) {
     res.status(400).json({ error: 'Missing body' });
     return;
   }
-  
+
   const { agentId, referralCode } = req.body;
-  
-  Client.findOne({
+
+  const client = await Client.findOne({
     where: { referralCode, agentId, isActive: true }
-  }).then(client => {
-    if (!client) {
-      res.json({ error: 'Client Not Found' }).status(404);
-    } else {
-      Agent.findOne({
-        where: { id: agentId }
-      }).then(agent => {
-        if (!agent) {
-          res.json({ error: 'Agent Not Found' }).status(404);
-        } else {
-          // dv: Todo - record event for client referral opening this invite
-          res.json({
-            invite: {
-              clientId: client.id,
-              clientName: client.firstName,
-              agentId: agent.id,
-              agentName: agent.firstName
-            }
-          });
-        }
-      });
+  });
+  if (!client) {
+    res.status(404).json({ error: 'Client not found' });
+    return;
+  }
+
+  const agent = await Agent.findOne({
+    where: { id: agentId }
+  });
+
+  if (!agent) {
+    res.status(404).json({ error: 'Agent not found' });
+    return;
+  }
+
+  res.json({
+    invite: {
+      clientId: client.id,
+      clientName: client.firstName,
+      agentId: agent.id,
+      agentName: agent.firstName
     }
   });
-  
-});
+
+}));
 
 module.exports = router;
