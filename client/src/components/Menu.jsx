ba558325951d2da7592c5e08ea093427a31238c2
Dejan Vasic <dvasic@seek.com.au>
2019-03-26 17:45:16 +1100

    Adding app name in to the menu

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,65 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
+import { Link } from 'react-router-dom';
+
+import { 
+  Divider,
+  List, 
+  ListItem, 
+  ListItemIcon,
+  ListItemText,
+  Typography
+} from '@material-ui/core';
+
 import HomeIcon from '@material-ui/icons/Home';
 import PeopleIcon from '@material-ui/icons/People';
 import UserIcon from '@material-ui/icons/Accessibility'
 import PowerOffIcon from '@material-ui/icons/PowerOffOutlined';
-import { Link } from 'react-router-dom';
+
+import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-    root: {
-        width: '100%',
-        maxWidth: 360,
-        backgroundColor: theme.palette.background.paper,
-    },
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  appName: {
+    fontSize: '1.2em'
+  }
 });
 
 function Menu(props) {
-    const { classes, onLogout } = props;
-    return (
-        <div className={classes.root}>
-            <List component="nav">
-                <ListItem component={Link} to="/app" button>
-                    <ListItemIcon>
-                        <HomeIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Home" />
-                </ListItem>
-                <ListItem component={Link} to="/app/clients" button>
-                    <ListItemIcon>
-                        <PeopleIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Clients" />
-                </ListItem>
-                <ListItem component={Link} to="/app/agent/details" button>
-                    <ListItemIcon>
-                        <UserIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="My Details" />
-                </ListItem>
-            </List>
-            <Divider />
-            <List>
-                <ListItem onClick={onLogout} button>
-                    <ListItemIcon>
-                        <PowerOffIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Logout" />
-                </ListItem>
-            </List>
-        </div>
-    );
+  const { classes, onLogout } = props;
+  return (
+    <div className={classes.root}>
+      <List component="nav">
+        <ListItem>
+          <Typography className={classes.appName}>
+            Fox Rewarder
+          </Typography>
+        </ListItem>
+        <Divider />
+        <ListItem component={Link} to="/app" button>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem component={Link} to="/app/clients" button>
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Clients" />
+        </ListItem>
+        <ListItem component={Link} to="/app/agent/details" button>
+          <ListItemIcon>
+            <UserIcon />
+          </ListItemIcon>
+          <ListItemText primary="My Account" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem onClick={onLogout} button>
+          <ListItemIcon>
+            <PowerOffIcon />
+          </ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
+      </List>
+    </div>
+  );
 }
 
 Menu.propTypes = {
-    classes: PropTypes.object.isRequired,
-    onLogout: PropTypes.func.isRequired
+  classes: PropTypes.object.isRequired,
+  onLogout: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(Menu);
