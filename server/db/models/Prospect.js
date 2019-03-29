134cb097cd485be52d57625b726d23e295a8e4cd
Alex Maslyukov <maslukov@gmail.com>
2019-03-29 12:05:47 +1100

    feat(Invitation) made form more friendlier (#27)

--- server/db/models/Prospect.js
+++ server/db/models/Prospect.js
@@ -1,45 +1,51 @@
 module.exports = (sequelize, type) => {
-  return sequelize.define('Prospects', {
-    id: {
-      type: type.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false
-    },
-    agentId: {
-      type: type.INTEGER,
-      references: {
-        model: 'Agents',
-        id: 'id'
+  return sequelize.define(
+    'Prospects',
+    {
+      id: {
+        type: type.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
       },
-      allowNull: false,
-    },
-    clientId: {
-      type: type.INTEGER,
-      references: {
-        model: 'Clients',
-        id: 'id'
+      agentId: {
+        type: type.INTEGER,
+        references: {
+          model: 'Agents',
+          id: 'id'
+        },
+        allowNull: false
       },
-      allowNull: false,
+      clientId: {
+        type: type.INTEGER,
+        references: {
+          model: 'Clients',
+          id: 'id'
+        },
+        allowNull: false
+      },
+      status: {
+        // dv: E.g. MeetingPending not sure how to do types
+        type: type.STRING,
+        allowNull: false
+      },
+      firstName: {
+        type: type.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: type.STRING,
+        allowNull: false
+      },
+      email: {
+        type: type.STRING,
+        allowNull: false
+      },
+      phone: type.STRING,
+      message: type.STRING,
+      createdAt: type.DATE,
+      updatedAt: type.DATE
     },
-    status: {  // dv: E.g. MeetingPending not sure how to do types 
-      type: type.STRING,
-      allowNull: false
-    },
-    firstName: {
-      type: type.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: type.STRING,
-      allowNull: false
-    },
-    email: {
-      type: type.STRING,
-      allowNull: false
-    },
-    phone: type.STRING,
-    createdAt: type.DATE,
-    updatedAt: type.DATE
-  }, {});
+    {}
+  );
 };
