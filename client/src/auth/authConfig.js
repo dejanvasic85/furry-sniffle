e19b66cb5512aa1168512cd91a6a17db7fc0745b
Dejan Vasic <dvasic@seek.com.au>
2019-05-01 12:47:08 +1000

    Adding fox rewarder auth0 configuration

--- client/src/auth/authConfig.js
+++ client/src/auth/authConfig.js
@@ -1,7 +1,7 @@
 export const authConfig = {
-  audience: 'http://localhost:5000',
-  domain: 'paramount.au.auth0.com',
-  clientId: 'vBP5pNdN3pg0AF7cPTd6UlqolKxbD335',
+  audience: 'https://www.foxrewarder.com.au/api',
+  domain: 'foxrewarder.au.auth0.com',
+  clientId: 'WgWzBgqXpTQn2DPJKlmYVllf6OHu3fjJ',
   callbackUrl:
     process.env.REACT_APP_AUTH_CALLBACK_URL || 'http://localhost:3000/callback'
 };
