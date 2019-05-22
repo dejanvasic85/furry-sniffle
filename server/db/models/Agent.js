6e9e5e0545f1e264179f28e269ed6de7bc2278b2
Dejan Vasic <dvasic@seek.com.au>
2019-05-22 17:23:42 +1000

    Fixing the accountId to be integer

--- server/db/models/Agent.js
+++ server/db/models/Agent.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, type) => {
     businessName: type.STRING,
     abn: type.STRING,
     phone: type.STRING,
-    accountId: type.STRING,
+    accountId: type.INTEGER,
     createdAt: type.DATE,
     updatedAt: type.DATE
   }, {});
