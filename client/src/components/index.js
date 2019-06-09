79f0f803cef7d23eaf06e4a5cebd2ddc2d603df4
Dejan Vasic <dvasic@seek.com.au>
2019-06-09 15:48:56 +1000

    feat(getAllClients): Adding clientsWithoutEmails count to endpoint (#81)
    * Fetching clientsWithoutEmails when getting all clients for Agent
    
    * no need for Email

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Alert } from './Alert';
 export { default as Button } from './Button';
 export { default as Currency } from './Currency';
 export { default as Loader } from './Loader';
+export { default as SearchInput } from './SearchInput';
