6dfda860bb665ca8942bc2360c733629f5f03df6
Dejan Vasic <dvasic@seek.com.au>
2019-07-02 18:28:54 +1000

    Adding data migration to the script

--- server/db/migrations/20190702074125-email-type.js.js
+++ server/db/migrations/20190702074125-email-type.js.js
@@ -2,6 +2,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Emails', 'emailType', {
       type: Sequelize.STRING
+    }).then(() => {
+      return queryInterface.sequelize.query(`UPDATE "Emails" set "emailType" = 'welcome_email'`);
     });
   },
 
